4a0747b9f681658f492794df37315782a7dbc227
Nandmin <andras78.nemeth@gmail.com>
2022-10-27 01:12:40 +0200

    Firebase publish

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebaseConfig : {
+    apiKey: "AIzaSyAoM5FazZE9bZXtpg9TxgLfGXLB9grtPGo",
+    authDomain: "eat-less-salt.firebaseapp.com",
+    projectId: "eat-less-salt",
+    storageBucket: "eat-less-salt.appspot.com",
+    messagingSenderId: "140113883747",
+    appId: "1:140113883747:web:4e0d4d3c00b41954eb3e56",
+    measurementId: "G-NLE0ZZTKGG"
+  },
   production: true
 };
